420e2cfb8f21aa8492801598d9be244994100f1e
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2018-01-18 15:31:00 -0500

    al;skjfl;kasj fd

--- src/input/DimitrisAlgebraicNode.java
+++ src/input/DimitrisAlgebraicNode.java
@@ -1,5 +1,27 @@
 package input;
 
 public abstract class DimitrisAlgebraicNode {
-
+	
+	public DimitrisAlgebraicNode lhs;
+	public DimitrisAlgebraicNode rhs;
+	
+	public double value;
+	public boolean isEvaluated;
+	
+	DimitrisAlgebraicNode(DimitrisAlgebraicNode lhs, DimitrisAlgebraicNode rhs){
+		this.lhs = lhs;
+		this.rhs = rhs;
+		this.isEvaluated = false;
+	}
+	
+	DimitrisAlgebraicNode(double value){
+		this.value = value;
+		this.isEvaluated = true;
+	}
+	
+	
+	
+	
+	
+	
 }
