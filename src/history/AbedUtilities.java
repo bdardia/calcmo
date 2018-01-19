1da05e39c3109c75b599cd960590e4a9f10df9e6
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2018-01-19 15:13:37 -0500

    sss

--- src/history/AbedUtilities.java
+++ src/history/AbedUtilities.java
@@ -1,11 +1,19 @@
 package history;
 
+import java.util.ArrayList;
+
 public class AbedUtilities
 {
-
-	public AbedUtilities() 
+	public boolean search(ArrayList<AbedHistoryNode> h,String t)
 	{
-		
+		for(int i = 0; i < h.size();i++)
+		{
+			if(h.get(i).print().get(0).equals(t))
+			{
+				return true;
+			}
+		}
+		return false; 
 	}
-
+	
 }
