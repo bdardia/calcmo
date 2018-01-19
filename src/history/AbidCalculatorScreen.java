3867a8b485a1ab7d439f203cb276b48401ff0027
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2018-01-19 14:45:24 -0500

    qsqs

--- src/history/AbidCalculatorScreen.java
+++ src/history/AbidCalculatorScreen.java
@@ -3,16 +3,16 @@ package history;
 import java.util.List;
 
 import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
 import guiTeacher.userInterfaces.Screen;
 
-public abstract class AbidCalculatorScreen extends Screen {
+public abstract class AbidCalculatorScreen extends FullFunctionScreen {
 
 	public AbidCalculatorScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
 	public abstract void initObjects(List<Visible> viewObjects);
 
 }
