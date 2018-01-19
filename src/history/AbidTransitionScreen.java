4f561e5880dc1e74d8d6ceb16299a999da7104a2
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2018-01-19 15:24:54 -0500

    A

---
+++ src/history/AbidTransitionScreen.java
@@ -0,0 +1,21 @@
+package history;
+
+import java.util.List;
+
+import guiTeacher.components.Graphic;
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class AbidTransitionScreen extends FullFunctionScreen{
+
+	public AbidTransitionScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		viewObjects.add(new Graphic(0, 0, getWidth(),getHeight(),"resources/beemo-transitionScreen"));
+	}
+
+}
