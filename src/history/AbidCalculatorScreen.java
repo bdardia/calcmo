8161ce5a1a943726628573b553e00a340fe92cf5
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-01-30 14:45:15 -0500

    uo

--- src/history/AbidCalculatorScreen.java
+++ src/history/AbidCalculatorScreen.java
@@ -1,18 +1,23 @@
 package history;
 
 import java.util.List;
-
 import guiTeacher.interfaces.Visible;
-import guiTeacher.userInterfaces.FullFunctionScreen;
 import guiTeacher.userInterfaces.Screen;
+import main.JasMain;
 
-public abstract class AbidCalculatorScreen extends FullFunctionScreen {
+public abstract class AbidCalculatorScreen extends JasMain implements SwitchScreen{
 
+	private AbidTransitionScreen transitionScreen;
 	public AbidCalculatorScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
 	}
 
 	public abstract void initAllObjects(List<Visible> viewObjects);
+	
+	public void switchScreen(Screen s) {
+		setScreen(transitionScreen);
+		setScreen(s);
+	};
 
 }
