6d98b044c9b36b259d81bc59f57550aa6d8f64b2
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-01-31 15:28:28 -0500

    started variable storage

--- src/input/BenInputScreen.java
+++ src/input/BenInputScreen.java
@@ -1,10 +1,13 @@
 package input;
 
+import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
 
 import guiTeacher.components.Action;
 import guiTeacher.components.Button;
 import guiTeacher.components.Graphic;
+import guiTeacher.components.TextLabel;
 import guiTeacher.interfaces.Visible;
 import history.AbidCalculatorScreen;
 import main.CalcMoMain;
@@ -16,6 +19,8 @@ public class BenInputScreen extends AbidCalculatorScreen
 	 */
 	private static final long serialVersionUID = 7132035530444379674L;
 	public static Graphic background;
+	public static TextLabel inputArea;
+	public static TextLabel currentArea;
 	public static Button limButton;
 	public static Button derivButton;
 	public static Button linsolveButton;
@@ -24,17 +29,21 @@ public class BenInputScreen extends AbidCalculatorScreen
 	public static Button exponentButton;
 	public static Button logSolveButton;
 	public static Button summationButton;
+	public static Button oneButton;
+	public static ArrayList<BenVariableStorage> variables;
 	
 	public BenInputScreen(int width, int height) 
 	{
 		super(width, height);
 	}
+	
+	// make arraylist for variable entry
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) 
 	{
 		background = new Graphic(0, 0, "resources/calcmoscreen.png");
-		
+		inputArea = new TextLabel(37, 37, 400, 37, "");
 		// add 7 to y value
 		limButton = new Button(60, 405, 26, 15, "", new Action() {
 			
@@ -92,7 +101,6 @@ public class BenInputScreen extends AbidCalculatorScreen
 				switchScreen(CalcMoMain.historyScreen);
 			}
 		});
-		// 60, 591, 63, 15
 		summationButton = new Button(60, 598, 70, 15, "", new Action() {
 			
 			@Override
@@ -101,11 +109,21 @@ public class BenInputScreen extends AbidCalculatorScreen
 				System.out.println("summation button pressed");
 			}
 		});
+		// 275, 382, 8, 15
+		oneButton = new Button(278, 385, 15, 15, "", new Action() {
+			
+			@Override
+			public void act() 
+			{
+				inputArea.setText(inputArea.getText() + "1");
+			}
+		});
 		
 		Thread cleanUp = new Thread(new BenSound());
 		Runtime.getRuntime().addShutdownHook(cleanUp);
 		
 		viewObjects.add(background);
+		viewObjects.add(inputArea);
 		viewObjects.add(limButton);
 		viewObjects.add(derivButton);
 		viewObjects.add(linsolveButton);
@@ -114,6 +132,7 @@ public class BenInputScreen extends AbidCalculatorScreen
 		viewObjects.add(historyButton);
 		viewObjects.add(logSolveButton);
 		viewObjects.add(summationButton);
+		viewObjects.add(oneButton);
 	}
 
 
---
+++ src/input/BenVariableStorage.java
@@ -0,0 +1,33 @@
+package input;
+
+public class BenVariableStorage 
+{
+	
+	private String name;
+	private int value;
+	
+	public BenVariableStorage(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public int getValue()
+	{
+		return this.value;
+	}
+	
+	public void setValue(int value)
+	{
+		this.value = value;
+	}
+}
