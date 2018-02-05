0cd22032819cf160c94afe3b226dfcc9466bec9c
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-02-05 15:17:23 -0500

    jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj

--- src/history/JasHistoryScreen.java
+++ src/history/JasHistoryScreen.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 import guiTeacher.components.*;
 import guiTeacher.interfaces.Visible;
+import input.BenInputScreen;
 import main.CalcMoMain;
 
 public class JasHistoryScreen extends AbidCalculatorScreen {
@@ -23,23 +24,15 @@ public class JasHistoryScreen extends AbidCalculatorScreen {
 		
 		viewObjects.add(new Graphic(0, 0, getWidth(),getHeight(),"resources/histMO.png"));
 		
-		Button goInput = new Button(150, 625, 130, 40, "Go Back", new Color(63, 72, 204), new Action() {
+		Button goInput = new Button(250, 575, 75, 50, "Go Back", new Color(237, 28, 36), new Action() {
 			
 			public void act() {
 				switchScreen(CalcMoMain.inputScreen);
 			}
 		});
+		JasCustomButton.circleButton(goInput);
 		viewObjects.add(goInput);
-		
-		Button select = new Button(375, 575, 85, 85,"Select", new Color(237, 28, 36), new Action() {
-			//should take the function clicked on by the user, return to the input screen, and enter the function as an input
-			public void act() {
 				
-			}
-		});
-		JasCustomButton.circleButton(select);
-		viewObjects.add(select);
-		
 		//triangle - shape
 		Button clear = new Button(375, 515, 93, 45, "Clear", new Color(153, 217, 234), new Action() {
 
@@ -51,25 +44,30 @@ public class JasHistoryScreen extends AbidCalculatorScreen {
 		});
 		viewObjects.add(clear);
 		
-		//make the array list all button so when the user clicks on it, it will return to the input screen with the function entered
 		ScrollablePane scroll = new ScrollablePane(this, 35, 39, 443, 275);
 		scroll.setBackground(new Color(165, 237, 186));	
 		for(int i = 0; i < fx.size(); i++){
 			
-			scroll.addObject(new Button(5,30*i,100,25,fx.get(i).getInput(), new Action() {
+			String in = fx.get(i).getInput();
+			String out = fx.get(i).getOutput();
+			
+			scroll.addObject(new Button(5, 30 * i + 10, 100, 25, in, new Action() {
 
 				public void act() {
+					BenInputScreen.inputArea.setText(in);
 					switchScreen(CalcMoMain.inputScreen);
 				}
 			}));	
 			
-			scroll.addObject(new Button(35,30*i,100,25,fx.get(i).getOutput(), new Action() {
+			scroll.addObject(new Button(35, 30 * i + 25, 100, 25, out, new Action() {
 
 				public void act() {
+					BenInputScreen.inputArea.setText(out);
 					switchScreen(CalcMoMain.inputScreen);
 				}
 			}));
 		}
+		
 		scroll.update();
 		
 		viewObjects.add(scroll);	
