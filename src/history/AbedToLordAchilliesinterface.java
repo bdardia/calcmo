d57ecf86bf0b3266452da69366ad5c779a31b909
nasir <nasir@nas1>
2018-02-07 15:02:33 -0500

    interfacres Abed

---
+++ src/history/AbedToLordAchilliesinterface.java
@@ -0,0 +1,9 @@
+package history;
+
+import java.util.ArrayList;
+
+public interface AbedToLordAchilliesinterface 
+{
+	///once you fill out the output simply add it to the arrayList the historyScreen owns
+	AbedHistoryNode addToList(AbedHistoryNode a, ArrayList<AbedHistoryNode> s);
+}
