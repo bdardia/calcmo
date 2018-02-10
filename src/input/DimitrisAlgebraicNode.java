1877c214dd6bc0cc57e12b409ae66a9a781e8416
deltaalpha33 <deltaalpha33@users.noreply.github.com>
2018-02-09 20:15:07 -0500

    added LhsParenthis and RhsParenthis to parser solver list

--- src/input/DimitrisAlgebraicNode.java
+++ src/input/DimitrisAlgebraicNode.java
@@ -16,7 +16,7 @@ public class DimitrisAlgebraicNode {
 	public String varName;
 	public boolean isVariable = false;
 	
-	public static Solver[] solverArray = {new AdditionSolver(), new MultiplicationSolver(), new DivisionSolver(), new SubtractionSolver()}; //set up in main
+	public static Solver[] solverArray = {new AdditionSolver(), new MultiplicationSolver(), new DivisionSolver(), new SubtractionSolver(), new LhsParenthesis(), new RhsParenthesis()}; //set up in main
 	
 	
 	
---
+++ src/input/RhsParenthesis.java
@@ -0,0 +1,39 @@
+package input;
+
+public class RhsParenthesis implements Solver {
+
+	public RhsParenthesis() {
+	}
+
+	@Override
+	public double solveNode(DimitrisAlgebraicNode lhs, DimitrisAlgebraicNode rhs) {
+		return 0;
+	}
+
+	@Override
+	public String getOperation() {
+		return ")";
+	}
+
+	@Override
+	public int getPrecedence() {
+		return -1;
+	}
+
+	@Override
+	public void addParent(DimitrisAlgebraicNode n) {
+
+	}
+
+	@Override
+	public Solver getInverse() {
+
+		return new LhsParenthesis();
+	}
+
+	@Override
+	public boolean urinaryFunction() {
+		return false;
+	}
+
+}
