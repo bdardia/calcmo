01393975a425705df6e286c682b12e06c6adaff1
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2018-02-12 14:21:00 -0500

    kyyy

--- src/output/SineSolver.java
+++ src/output/SineSolver.java
@@ -3,36 +3,37 @@ package output;
 import input.DimitrisAlgebraicNode;
 import input.Solver;
 
-public class PowerSolver implements Solver {
-
+public class SineSolver implements Solver {
+	
+	int precedence = Solver.PrecedenceConstants.sineSolver;
 	@Override
 	public double solveNode(DimitrisAlgebraicNode lhs, DimitrisAlgebraicNode rhs) {
-		// TODO Auto-generated method stub
-		return Math.pow(lhs.solveNode(), rhs.solveNode());
+		rhs.solve();
+		return java.lang.Math.sin(rhs.value);
 	}
 
 	@Override
 	public String getOperation() {
 		// TODO Auto-generated method stub
-		return "^";
+		return null;
 	}
 
 	@Override
 	public int getPrecedence() {
 		// TODO Auto-generated method stub
-		return 5;
+		return 0;
 	}
 
 	@Override
 	public Solver getInverse() {
 		// TODO Auto-generated method stub
-		return new MultiplicationSolver();
+		return null;
 	}
 
 	@Override
-	public void addParent(DimitrisAlgebraicNode n) {
+	public Solver createNew() {
 		// TODO Auto-generated method stub
-		
+		return null;
 	}
 
 	@Override
@@ -40,5 +41,11 @@ public class PowerSolver implements Solver {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
+
+	@Override
+	public void increasePrecedence(int amount) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
