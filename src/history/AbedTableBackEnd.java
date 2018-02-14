bb22885305afbf4c9b68f31d67ee87abf24d49e8
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2018-02-14 15:27:32 -0500

    dfdf

--- src/history/AbedTableBackEnd.java
+++ src/history/AbedTableBackEnd.java
@@ -2,16 +2,19 @@ package history;
 
 import java.util.ArrayList;
 
+import input.DimitrisNodeBuilder;
+
 public class AbedTableBackEnd 
 {
 	private ArrayList<AbedHistoryNode> functionNodes = JasHistoryScreen.fx;
 	private ArrayList<String> domain;
-	private ArrayList
+	///compile on the string caloled first n.solve then fill
+	private DimitrisNodeBuilder s;
 	
 	
 	public AbedTableBackEnd() 
 	{
-		
+		s = new DimitrisNodeBuilder();
 	}
 	private void constructOutputs()
 	{
