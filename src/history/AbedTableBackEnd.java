9f963fcb4267293711cec354c2f0829fffecbb4f
fluffy <fluffy@192.168.0.19>
2018-02-14 21:24:42 -0500

    xasx

--- src/history/AbedTableBackEnd.java
+++ src/history/AbedTableBackEnd.java
@@ -2,23 +2,31 @@ package history;
 
 import java.util.ArrayList;
 
+import input.DimitrisAlgebraicNode;
 import input.DimitrisNodeBuilder;
 
 public class AbedTableBackEnd 
 {
 	private ArrayList<AbedHistoryNode> functionNodes = JasHistoryScreen.fx;
 	private ArrayList<String> domain;
-	///compile on the string caloled first n.solve then fill
-	private DimitrisNodeBuilder s;
+	///compile on the string called first n.solve then fill
 	
-	
+	public static void main(String[] args)
+	{
+		createOutputs();
+	}
 	public AbedTableBackEnd() 
 	{
 		s = new DimitrisNodeBuilder();
-	}
-	private void constructOutputs()
-	{
 		
 	}
-
+	public static void createOutputs()
+	{
+		 DimitrisAlgebraicNode n = DimitrisNodeBuilder.compileProgram("x^2");
+		 DimitrisNodeBuilder.updateVariable(n, "x", 0);
+		 n.solve();
+		 System.out.println(n.value);
+		
+	}
+	
 }
